044bff3ff2618580380f1c5d4c606f00962ceeb6
HuyQuang2601 <huyxauzai2601@gmail.com>
2024-11-27 13:28:33 +0700

    Add files via upload

--- Session06_Ex5.c
+++ Session06_Ex5.c
@@ -8,10 +8,18 @@ int main(){
 	scanf("%d", &month);
 	if(year>=2000){
 		switch(month){
-			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			case 1: 
+			case 3: 
+			case 5: 
+			case 7: case 8:
+			case 10:
+			case 12:
 				printf("Thang nay co 31 ngay");
 				break;
-			case 4: case 6: case 9: case 11:
+			case 4: 
+			case 6: 
+			case 9: 
+			case 11:
 				printf("Thang nay co 30 ngay");
 				break;
 			case 2:
